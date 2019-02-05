97e44f82aa5d59ec4bf131cafc2831075c1feec9
kursiel <39704600+kursiel@users.noreply.github.com>
2019-02-05 13:45:07 -0600

    Add files via upload

---
+++ ViewModelUserEmail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FormMultipleInsert.Models
+{
+    public class ViewModelUserEmail
+    {
+        [Required(ErrorMessage = "This field is requiered")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is requiered")]
+
+        public string Password { get; set; }
+
+        [DisplayName("Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "This field is requiered")]
+        public string EmailAddress { get; set; }
+        public bool IsAdmin { get; set; }
+
+
+
+    }
+}
---
+++ ViewModelUserEmailController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FormMultipleInsert.Models;
+
+namespace FormMultipleInsert.Controllers
+{
+    public class ViewModelUserEmailController : Controller
+    {
+        // GET: ViewModelUserEmail
+
+        [HttpGet]
+        public ActionResult Add(int id = 0)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(ViewModelUserEmail viewModel)
+        {
+            using (UserRegistrationEntities DbModel = new UserRegistrationEntities())
+            {
+                User userModel = new User()
+                {
+                    Username = viewModel.Username,
+                    Password = viewModel.Password,
+                    IsAdmin = viewModel.IsAdmin
+                };
+
+                DbModel.Users.Add(userModel);
+                DbModel.SaveChanges();
+
+                User userId = DbModel.Users.FirstOrDefault(x => x.Username == viewModel.Username);
+
+                Email emailModel = new Email()
+                {
+                    UserId = userId.Id,
+                    EmailAddress = viewModel.EmailAddress,
+                    User = userId 
+                };
+
+                DbModel.Emails.Add(emailModel);
+                DbModel.SaveChanges();
+            }
+                
+
+            return View();
+        }
+    }
+}
